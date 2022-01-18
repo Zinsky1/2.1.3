85113d18f6abc562c28b76b9b338b0a95cd9ba3f
Bolotov Alexander <distalker1@gmail.com>
2022-01-18 08:42:56 +0300

    fix pom

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,18 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        boolean b1 = bean == bean2;
+        System.out.println(b1);
+
+        Cat cat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        boolean b2 = cat1 == cat2;
+        System.out.println(b2);
     }
 }
